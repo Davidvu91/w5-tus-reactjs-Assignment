007622afdd0d504d12cdf3107fc9876a14f3a07c
Vu_Hong_Son <sonvuhong177@gmail.com>
2021-07-20 22:45:56 +0700

    Create a Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import {useState} from 'react';
+
 
 function App() {
+
+  const [factor, setFactor] = useState({
+    yourName: '',
+    email: '',
+    phone: '',
+    password: '',
+    retypePassword: '',
+  })
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      if(factor.password!==factor.retypePassword) {
+        alert('Your password not match')
+      }
+  };
+
+
+
+const handleFunction = (event) => {
+
+  setFactor({...factor, [event.target.name]: event.target.value});
+
+}
+console.log(factor);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello Jaguar</h1>
+
+      <form onSubmit={handleSubmit} id="form">
+
+        <label htmlFor="emai">Name:</label><br/>
+        <input type="text" id="emai" name="yourName" onChange={handleFunction} /><br/>
+
+        <label htmlFor="password">Email:</label><br/>
+        <input type="mail" id="password" name="email" onChange={handleFunction} /><br/>
+
+        <label htmlFor="password">Phone No:</label><br/>
+        <input type="text" id="password" name="phone" onChange={handleFunction}/><br/>
+
+        <label htmlFor="password">Password:</label><br/>
+        <input type="password" id="password" name="password" onChange={handleFunction} /><br/>
+
+        <label htmlFor="password">Retype Password:</label><br/>
+        <input type="password" id="password" name="retypePassword" onChange={handleFunction} /><br/><br/>
+
+        <button type="submit"> Submit </button>
+
+      </form> 
+
     </div>
   );
 }
+
 
 export default App;
